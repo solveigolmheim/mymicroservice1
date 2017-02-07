81006d9c8c3c23ee45beba56ad19f47035576a87
Solveig Ølmheim <solveigolmheim@gmail.com>
2017-02-07 13:55:22 +0100

    initial version

---
+++ src/main/java/com/solveig/Mymicroservice1Application.java
@@ -0,0 +1,12 @@
+package com.solveig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mymicroservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mymicroservice1Application.class, args);
+	}
+}
